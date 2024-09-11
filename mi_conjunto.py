54e25467532b01e7d0d931deb94fd80c014e6d0c
AlexisJimenez14 <a.22308051281213@cbtis128.edu.mx>
2024-09-11 07:47:03 -0600

    Add files via upload

---
+++ mi_conjunto.py
@@ -0,0 +1,6 @@
+print("ejemplo de conjuntos set")
+tipodeportes={"futbol","tenis","basquetball"}
+print(tipodeportes)
+print("lista elementos del conjunto tipodeportes loop for")
+for jimenez in tipodeportes:
+    print(jimenez)
---
+++ mi_diccionario.py
@@ -0,0 +1,9 @@
+carro={
+"marca": "ferrari",
+"modelo": "xs+",
+"año": "2021"
+}
+print(carro)
+
+for x in carro:
+    print(x, carro[x])
